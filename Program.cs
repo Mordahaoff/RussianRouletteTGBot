e65680cd049b0bb59b686f79780da9a5cefda913
Mordahaoff <artemiymorev2016@gmail.com>
2025-06-07 19:51:20 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Host host = new Host("6610715284:AAHIBRkS9W-gOeby89ZRpHH5QgAGMeGlZtU");
+        host.Start();
+        Console.ReadLine();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RussianRouletteTGBot
+
